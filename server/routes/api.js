62d8dc2a82a67e5036cbfb5eae1fe0c0e6f3ca35
Shervinee <shervinee@gmail.com>
2017-09-21 13:22:36 -0400

    finalized the api.js for handling the GET  method

--- server/routes/api.js
+++ server/routes/api.js
@@ -8,3 +8,36 @@ const router = express.Router();
 const MongoClient = require('mongodb').MongoClient;
 const ObjectID = require('mongodb').ObjectID; //our ObjectID of our monogoDB databases 
 
+//url of our local host for MongoDB 
+var url = 'mongodb://localhost:27017/mean';
+
+//setting up our connection 
+const connect = (closure) => {
+    MongoClient.connect(url, (err,db) => {
+        if(err) return console.log(err);
+
+        closure(db);
+    });
+};
+
+//making response object handling our response 
+let response = {
+    status: 200,
+    data: [],
+    message: null
+};
+
+router.get('/users', (req,res) => {
+    connect((db) => {
+        db.collection('users')
+        .find()
+        .toArray()
+        .then((users) => {
+            response.data = users;
+            res.json(response);
+        })
+        .catch((err) => {
+            sendError(err, res);
+        });
+    });
+});
